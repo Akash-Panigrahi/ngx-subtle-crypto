b566cffb084635c4eb35e1d50634be0fe1cba13f
Akash-Panigrahi <akashpanigrahi96@gmail.com>
2019-04-06 18:27:43 +0530

    use ngx-subtle-crypto's default component in ngx-subtle-crypto-app application

--- projects/ngx-subtle-crypto-app/src/app/app.module.ts
+++ projects/ngx-subtle-crypto-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxSubtleCryptoModule } from 'ngx-subtle-crypto';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSubtleCryptoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
