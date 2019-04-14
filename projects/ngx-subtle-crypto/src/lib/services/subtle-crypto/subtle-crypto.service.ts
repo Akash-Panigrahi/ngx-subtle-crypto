f1a77ae69d16e1254192428c661f108226b60cf3
Akash-Panigrahi <akashpanigrahi96@gmail.com>
2019-04-14 21:33:18 +0530

    add new utils.ts file that will keep all the required utility functions

--- projects/ngx-subtle-crypto/src/lib/services/subtle-crypto/subtle-crypto.service.ts
+++ projects/ngx-subtle-crypto/src/lib/services/subtle-crypto/subtle-crypto.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { of, Observable, from } from 'rxjs';
+import { Observable, from } from 'rxjs';
+
+import { stringToArrayBuffer, arrayBufferToString } from '../utils';
 
 @Injectable({
   providedIn: 'root'
@@ -8,24 +10,9 @@ export class SubtleCryptoService {
 
   constructor() { }
 
-  private _stringToArrayBuffer(str: string): ArrayBuffer {
-    const buffer = new ArrayBuffer(str.length * 2); // 2 bytes for each array
-    const bufferView = new Uint16Array(buffer);
-
-    for (let i = 0; i < str.length; i++) {
-      bufferView[i] = str.charCodeAt(i);
-    }
-
-    return buffer;
-  }
-
   digest(algorithm: string = 'SHA-256', data: string): Observable<string> {
-    const digestValue = window.crypto.subtle.digest(algorithm, this._stringToArrayBuffer(data));
+    const digestValue = window.crypto.subtle.digest(algorithm, stringToArrayBuffer(data));
     console.log(digestValue);
-    return from(digestValue.then(this._arrayBufferToString));
-  }
-
-  private _arrayBufferToString(ab: ArrayBuffer): string {
-    return String.fromCharCode.apply(null, new Uint16Array(ab));
+    return from(digestValue.then(arrayBufferToString));
   }
 }
